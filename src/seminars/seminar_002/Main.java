ca60ac5cc280363d13e6dd819e49b803dd72f27b
«Andrew» <>
2023-10-15 15:46:27 +0700

    added seminar 002

--- src/seminars/seminar_002/Main.java
+++ src/seminars/seminar_002/Main.java
@@ -2,6 +2,11 @@ package seminars.seminar_002;
 
 public class Main {
     public static void main(String[] args) {
-
+        Market market = new Market();
+        Human human1 = new Human("Ivan");
+        Human human2 = new Human("Vladimir");
+        market.acceptToMarket(human1);
+        market.acceptToMarket(human2);
+        market.update();
     }
 }
---
+++ src/seminars/seminar_002/QueueBehaviour.java
@@ -0,0 +1,8 @@
+package seminars.seminar_002;
+
+public interface QueueBehaviour {
+    void takeInQueue(Actor actor);
+    void takeOrders();
+    void giveOrders();
+    void releaseFromQueue();
+}
